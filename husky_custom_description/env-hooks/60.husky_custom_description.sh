af961a99c2e51f55e828a640598ebb494f6a8dbb
Rick Armstrong <rarmstrong@quantumspatial.com>
2018-04-05 18:18:01 -0700

    Added env vars for IMU position and orientation.RWL-492

--- husky_custom_description/env-hooks/60.husky_custom_description.sh
+++ husky_custom_description/env-hooks/60.husky_custom_description.sh
@@ -1,1 +1,3 @@
 export HUSKY_DESCRIPTION=$(catkin_find --first-only --without-underlays husky_custom_description urdf/custom_description.urdf.xacro 2>/dev/null)
+export HUSKY_IMU_XYZ="0 0.025 0.1143"
+export HUSKY_IMU_RPY="0 0 0"
